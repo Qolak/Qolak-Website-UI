dd9c2f1d9fb544349c42f5ae2ec36b94a9b5dca1
Psyringe <psyringe@yandex.com>
2015-09-03 18:55:21 +0430

    Added email verification using JavaScript before submitting form.

--- js/qolak.js
+++ js/qolak.js
@@ -30,6 +30,12 @@ function checkForm()
 		Materialize.toast('<a class=btn-flat href=# onclick=removeElement(&#39;toast-container&#39;) >X</a><span>رمزهای عبور شبیه به هم نیستند! لطفا دوباره وارد کنید.</span>', 3000);
 	}
 
+	//Check if email has the correct syntax
+	else if(!isEmail($('#email').val()))
+	{
+		Materialize.toast('<a class=btn-flat href=# onclick=removeElement(&#39;toast-container&#39;) >X</a><span>ایمیل وارد شده صحیح نیست! لطفا دوباره وارد کنید.</span>', 3000);
+	}
+
 	else
 	{
 		$("#agreementModal").openModal();
@@ -43,4 +49,9 @@ function submitForm()
 
 function removeElement(id) {
     return (elem=document.getElementById(id)).parentNode.removeChild(elem);
+}
+
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
 }
