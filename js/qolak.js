34118b4dc91cea3c040e585e6f50b7a891fb7fb8
Psyringe <psyringe@yandex.com>
2015-09-03 17:51:26 +0430

    Added the whole registeration form

--- js/qolak.js
+++ js/qolak.js
@@ -5,5 +5,29 @@ $(document).ready(function() {
 		$("input.search-field").val(this.value);
 		$("input#search-field").val(this.value);
 	});
-	
+
 });
+
+function checkForm()
+{
+	var counter = 0;
+	$(".input-field input").each(function(index, el) {
+		if( $(this).val() != "" )
+		{
+			counter++;
+		}
+	});
+
+	if(counter==5)
+	{
+		$("form").submit();
+	}
+	else
+	{
+		Materialize.toast('<a class=btn-flat href=# onclick=removeElement(&#39;toast-container&#39;) >X</a><span>لطفا تمامی اطلاعات را به صورت کامل وارد کنید!</span>', 3000);
+	}
+}
+
+function removeElement(id) {
+    return (elem=document.getElementById(id)).parentNode.removeChild(elem);
+}
