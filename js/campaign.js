258480fe3d753392de570b3e83f913676fcda639
mehrdad <mehrdad@admin>
2015-09-20 12:46:02 +0430

    tabs bug in resize fixed

--- js/campaign.js
+++ js/campaign.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-    
+
+	$("#com").addClass('active');
+    $(".side-awards").clone().appendTo(".tab-awards");
+	$(".tab-awards .side-awards").removeClass('col l4 hide-on-med-and-down').addClass('hide-on-large-only')
+ 
+    resize_bugfix();
+
  });
+
+function resize_bugfix() {
+	// Cache reference to our container
+    var $container = $("#container");
+    var $lastStatus=0;
+    // A function for updating max-height
+    function updateMaxHeight () {
+    	if($("#award").css("display")!="block") {	
+    		if($lastStatus==1) {
+    			$("#qolak").click();
+    			$lastStatus=0;
+    		}
+    	}else{
+    		$lastStatus=1;
+    	}
+    }
+    // Call updateMaxHeight when browser resize event fires
+    $(window).on("resize", updateMaxHeight);
+}
