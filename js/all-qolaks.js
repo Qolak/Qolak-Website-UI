23bf0d967dbffc9b3d18e1c3dbd9740fc7163ae5
mehdad <mehdad@admin>
2015-10-01 16:22:32 +0330

    all qolaks sort phase 1 added

--- js/all-qolaks.js
+++ js/all-qolaks.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     //$('.sort-by-button').dropdown();
     $('select').material_select();
+
+    $('#Container').mixItUp();
+	$('#filters').on('change', function() { 
+		//alert(this.value);
+	   $('#Container').mixItUp('filter',this.value);
+	});
 });
