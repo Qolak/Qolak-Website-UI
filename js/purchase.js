b4ef432cd9aec237dfd19af8fca86a5324a15b1e
mehrdad <m.mohamadi313@gmail.com>
2015-10-09 19:56:58 +0330

    step navigation added

--- js/purchase.js
+++ js/purchase.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-
+jQuery(document).ready(function() {
+    jQuery('.move-step').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        jQuery('.steps ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
 });
