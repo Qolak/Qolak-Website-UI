26087c430c7b89c98abb7e7d3e8f371bb20a9ac6
mehrdad <m.mohamadi313@gmail.com>
2015-10-16 22:14:14 +0330

    pushpin of categories section added

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('.card.short-post').clone().appendTo('.posts-container');
 	$('.card.short-post').clone().appendTo('.posts-container');
-	$('.card.short-post').clone().appendTo('.posts-container');
-	$('.card.short-post').clone().appendTo('.posts-container');
+
+	$('.indigo.lighten-5 .posts-category').pushpin({ top: $('.indigo.lighten-5').offset().top-120});
 });
