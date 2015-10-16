e55f41cae25ebad613bbcedba399db529228f5c3
mehrdad <m.mohamadi313@gmail.com>
2015-10-16 20:07:29 +0330

    blog.js added to project

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('.card.short-post').clone().appendTo('.posts-container');
+	$('.card.short-post').clone().appendTo('.posts-container');
+	$('.card.short-post').clone().appendTo('.posts-container');
+	$('.card.short-post').clone().appendTo('.posts-container');
+});
