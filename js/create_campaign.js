4272cf8e9a5b63cb21667f479a5d10e97f9dd7c4
Psyringe <psyringe@yandex.com>
2015-10-21 20:21:30 +0330

    Fixed breadcrumbs. Now they work like a box.

--- js/create_campaign.js
+++ js/create_campaign.js
@@ -13,8 +13,26 @@ $(document).ready(function() {
         e.preventDefault();
         $.scrollTo($(targetTab).offset().top-160, 800);
     });
-    $('.checkout-btn').click(function(){
-    	$('#checkout-li').addClass('done');
+    $('.back-step').on('click', function(e)  {
+        var targetTab = $(this).attr('href');
+        // Show/Hide Tabs
+        $('.steps ' + targetTab).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        $(".progress-nav " + targetTab +'-li').addClass('done').next().removeClass('done').next().removeClass('done');
+        e.preventDefault();
+        $.scrollTo($(targetTab).offset().top-160, 800);
+    });
+    $('.done-btn').click(function(){
+    	$('#done-li').addClass('done');
+    });
+
+    $('.q-clickable').on('click', function(e){
+        $(this).addClass('done').nextAll().removeClass('done');
+        var name = $(this).attr('id');
+        name = name.replace("-li", "");
+        $('.steps ' + name).show().siblings().hide();
+        e.preventDefault();
     });
 
 });
